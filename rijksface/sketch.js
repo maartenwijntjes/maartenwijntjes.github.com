f5fe52702e876e6de13bbf1a2f95110ba01faee3
maartenwijntjes <m.w.a.wijntjes@tudelft.nl>
2018-10-26 23:03:55 +0200

    rijks

--- rijksface/sketch.js
+++ rijksface/sketch.js
@@ -3,13 +3,13 @@ var image1,image2;
 var w1,w2,wString;
 
 function setup() {
-  createCanvas(512, 512);
+  createCanvas(800, 800);
   image1 = loadImage("meanfaceLeft.png");
   image2 = loadImage("meanfaceRight.png");
 }
 
 function draw() {
-  w1=float(mouseX)/512;
+  w1=float(mouseX)/800;
   w2=1-w1;
   //wString=str(round(w1*100)/100);
   tint(255, 255, 255, 255*w2);
