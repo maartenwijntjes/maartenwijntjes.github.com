ba30ed9f3eb9b24dcd015de97ed16cfa428ff8aa
maartenwijntjes <m.w.a.wijntjes@tudelft.nl>
2018-10-27 00:01:42 +0200

    rijks

--- rijksface/sketch.js
+++ rijksface/sketch.js
@@ -13,9 +13,9 @@ function draw() {
   w2=1-w1;
   //wString=str(round(w1*100)/100);
   tint(255, 255, 255, 255*w2);
-  image(image1, 0, 0);
+  image(image1, 0, 0,800,800);
   
   tint(255, 255, 255, 255*w1);
-  image(image2, 0, 0);
+  image(image2, 0, 0,800,800);
   
 }
