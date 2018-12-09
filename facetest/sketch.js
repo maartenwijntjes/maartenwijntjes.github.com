4c4afddd725d92652092fc0d31abecfe30740016
maartenwijntjes <m.w.a.wijntjes@tudelft.nl>
2018-12-09 21:25:05 +0100

    added javascript test demos.md

---
+++ facetest/sketch.js
@@ -0,0 +1,21 @@
+var image1,image2;  
+
+var w1,w2,wString;
+
+function setup() {
+  createCanvas(800, 800);
+  image1 = loadImage("facetest/meanfaceLeft.png");
+  image2 = loadImage("facetest/meanfaceRight.png");
+}
+
+function draw() {
+  w1=float(mouseX)/800;
+  w2=1-w1;
+  //wString=str(round(w1*100)/100);
+  tint(255, 255, 255, 255*w2);
+  image(image1, 0, 0,800,800);
+  
+  tint(255, 255, 255, 255*w1);
+  image(image2, 0, 0,800,800);
+  
+}
