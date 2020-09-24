7cb37352b57179b2cc560ae00c4c7756db70cdd1
maartenwijntjes <m.w.a.wijntjes@tudelft.nl>
2020-09-24 14:41:04 +0200

    additions

--- sketch.js
+++ sketch.js
@@ -1,6 +1,45 @@
 // save this file as sketch.js
 // Sketch One
-var s = function( p ) { // p could be any variable name
+
+let face = function(p){
+
+var ims = [];
+var ii = 0
+
+// This sketch loads in images, and then displays each image in rapid succession.
+
+
+  p.preload = function() {
+    let images = 82
+    for (let i = 1; i <= images; i++) {
+      path = 'demoYaw/' + p.str(i) + '.jpg' // create a path to the image
+      loaded_image = p.loadImage(path)     // load the image from the path
+      ims.push(loaded_image)             // add the loaded path to ims
+    }
+  }
+
+   p.setup= function()  {
+    // create a canvas, with the size and height of the first image
+    
+    p.createCanvas(500, 300)
+    
+  }
+
+   p.draw= function()  {
+    p.background(0);
+     
+     let k = p.int(p.map(p.mouseX,p.width,0,0,ims.length-1,true));
+     p.stroke(255)
+     p.text(k,20,20);
+    p.imageMode(p.CENTER);
+    p.image(ims[k], p.width/2, p.height/2,200,200); // show the ii-th image in the canvas
+    
+  }
+}
+let myp5 = new p5(face,'c1')
+
+
+/*var s = function( p ) { // p could be any variable name
   var x = 100; 
   var y = 100;
   p.setup = function() {
@@ -14,6 +53,7 @@ var s = function( p ) { // p could be any variable name
   };
 };
 var myp5 = new p5(s, 'c1');
+*/
 
 // Sketch Two
 var t = function( p ) { 
